72f571e856fa36943f4e86ea124f302bb0e3cba1
odan <odan@users.noreply.github.com>
2019-04-23 14:38:05 +0200

    Update cs

--- .cs.php
+++ .cs.php
@@ -24,14 +24,20 @@ return PhpCsFixer\Config::create()
             ],
         ],
         'increment_style' => ['style' => 'post'],
-        'list_syntax' => ['syntax' => 'short'],
+        'list_syntax' => ['syntax' => 'long'],
         'no_short_echo_tag' => true,
         'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
         'phpdoc_align' => false,
         'phpdoc_no_empty_return' => false,
         'phpdoc_order' => true, // psr-5
+        'phpdoc_no_useless_inheritdoc' => false,
         'protected_to_private' => false,
         'yoda_style' => false,
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'const', 'function']
+        ],
     ])
     ->setFinder(PhpCsFixer\Finder::create()
         ->in(__DIR__ . '/src')
